730e58754accfd1f171106b2867e41f619d847c1
David Gomesh <davidgomesh2016@gmail.com>
2023-11-01 15:52:16 -0300

    Add Content-Security-Policy

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <meta name="description" content="Page for managing subscriptions and event frequency for NAPNE events.">
     <meta name="author" content="David Gomesh">
