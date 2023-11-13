1516a296e9df36e85bb99c682541bf151a9af9b6
David Gomesh <davidgomesh2016@gmail.com>
2023-11-13 09:20:06 -0300

    Remove duplicated participants error

--- app/Service/ParticipantService.php
+++ app/Service/ParticipantService.php
@@ -6,6 +6,10 @@ use App\Models\Participant;
 
 class ParticipantService {
     public static function save(array $participant) {
+        if (Participant::where('name', $participant['name'])->first()) {
+            throw new \Exception('Participante já cadastrado!');
+        }
+
         return Participant::create(
             $participant + ['participant_id' => uuid_create()]
         );
